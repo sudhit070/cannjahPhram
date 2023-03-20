83f068cf21fdb25d55775fe0660f82f70e7c3500
manav <manav.darji@pocketfm.com>
2023-03-20 18:51:42 +0530

    adding gunicorn dependency

--- cannjahPhram/settings.py
+++ cannjahPhram/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sslserver',
+    'corsheaders',
     'App_User',
     'rest_framework',
 ]
@@ -41,7 +42,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:8000',
+)
 
 ROOT_URLCONF = 'cannjahPhram.urls'
 
