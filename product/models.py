444e41a5338b75b48ca9ccda717b4d63008d2b20
manav <manav.darji@pocketfm.com>
2023-04-04 21:49:04 +0530

    adding more fields to Product model

--- product/models.py
+++ product/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from common.constants import PRODUCT_STATE
 
 # Create your models here.
+class ProductCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    category_title = models.CharField(max_length=200, null=True, blank=True)
+
+class ProductImage(models.Model):
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to='media/products')
+
 class AvailableLabel(models.Model):
     id = models.AutoField(primary_key=True)
     tag_title = models.CharField(max_length=500, null=True, blank=True)
@@ -25,7 +33,9 @@ class Product(models.Model):
     aroma = models.CharField(max_length=1000, null=True, blank=True)
     terpenes = models.CharField(max_length=1000, null=True, blank=True)
     origin = models.CharField(max_length=500, null=True, blank=True)
-    product_state = models.CharField(max_length=100, choices=PRODUCT_STATE)
+    product_category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True, blank=True)
+    product_main_image = models.ForeignKey(ProductImage, on_delete=models.CASCADE, null=True, blank=True)
+    product_more_image = models.ManyToManyField(ProductImage, related_name="product_more_image")
 
     #available in tags for reference
     available_in_tags = models.ManyToManyField(AvailableLabel)
