022a674ddf6ac47649e001baf4bd2eebd1f8df90
manav <manav.darji@pocketfm.com>
2023-04-25 00:05:45 +0530

    changing the admin endpoint

--- cannjahPhram/urls.py
+++ cannjahPhram/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('cannjah_admin/', admin.site.urls),
     path('', include('App_User.urls')),
     path('medical/', include('medical.urls')),
 ]
