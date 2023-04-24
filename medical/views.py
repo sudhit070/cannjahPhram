18626577b55c8ab007d5cbe12f6fb19630c6f2cd
manav <manav.darji@pocketfm.com>
2023-04-24 23:54:22 +0530

    handling error/not valid response

--- medical/views.py
+++ medical/views.py
@@ -1,11 +1,12 @@
 import json
 from rest_framework.views import APIView
-from medical.serializer import EmailValidation
+from medical.serializer import EmailValidation, MedicalRegisterSerializer
 from rest_framework.serializers import ValidationError
 from rest_framework.response import Response
 from rest_framework import status
 from medical.models import MedicalRegister
 # Create your views here.
+
 class MedicalRegister(APIView):
     def get(self, request):
         try:
@@ -15,7 +16,7 @@ class MedicalRegister(APIView):
             serialized_data = serializer.validated_data
             email = serialized_data["email"]
             if MedicalRegister.objects.filter(email=email).first():
-                return Response(status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, data={"details": "Email already Registered", "accepted": False})
+                return Response(status=status.HTTP_400_BAD_REQUEST, data={"details": "Email already Registered!", "accepted": False})
             else:
                 return Response(status=status.HTTP_200_OK, data={"details": "No Email Found", "accepted": True})
         except ValidationError as e:
@@ -24,7 +25,7 @@ class MedicalRegister(APIView):
     def post(self, request):
         try:
             data = json.loads(request.body.decode('utf-8'))
-            serializer = EmailValidation(data=data)
+            serializer = MedicalRegisterSerializer(data=data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(status=status.HTTP_200_OK, data={"details": "Registered Successfull, Please check you're email for furthur process"})
