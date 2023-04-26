851fd68f04e83563c6968a6a5a920dade81bb0f3
sudhit070 <123994213+sudhit070@users.noreply.github.com>
2023-04-27 02:43:50 +0530

    Add files via upload

--- medical/views.py
+++ medical/views.py
@@ -7,10 +7,12 @@ from rest_framework import status
 from medical.models import MedicalRegister
 # Create your views here.
 
+
 class MedicalRegisterView(APIView):
     def get(self, request):
         try:
-            data = json.loads(request.body.decode('utf-8'))
+            data = {'email': request.GET.get('email')}
+            # data = json.loads(request.body.decode('utf-8'))
             serializer = EmailValidation(data=data)
             serializer.is_valid(raise_exception=True)
             serialized_data = serializer.validated_data
@@ -25,7 +27,7 @@ class MedicalRegisterView(APIView):
     def post(self, request):
         try:
             data = json.loads(request.body.decode('utf-8'))
-            serializer = MedicalRegisterSerializer(data=data)
+            serializer = MedicalRegisterSerializer(data=data, partial=True)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(status=status.HTTP_200_OK, data={"details": "Registered Successfull, Please check you're email for furthur process"})
